7cd460de901ee677b7d3bd60a2d08e31eb02e706
KMBarber1 <katherine.m.barber@outlook.com>
2022-05-10 23:09:54 -0700

    initial submit

--- src/App.js
+++ src/App.js
@@ -3,21 +3,15 @@ import './App.css';
 
 function App() {
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello Dojo</h1>
+      <h3>Things I need to do:</h3>
+        <li>Finish MERN</li>
+        <li>Graduate</li>
+        <li>Complete Resume</li>
+        <li>Apply for Jobs</li>
+      
     </div>
   );
 }
